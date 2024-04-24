6a6eb6db5897733694283690bec6bc9e5a0a5bf0
marijan.stanojevic <marijan.stanojevic@yahoo.com>
2024-04-24 08:37:31 +0200

    Included missing TastyTrails.Common project

---
+++ TastyTrails.Common/Attributes/Validation/NotNullOrWhiteSpaceAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TastyTrails.Common.Attributes.Validation
+{
+    public class NotNullOrWhiteSpaceAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is string valueString && !string.IsNullOrWhiteSpace(valueString))
+            {
+                return ValidationResult.Success;
+            }
+
+            return new ValidationResult($"{validationContext.DisplayName} cannot be null, empty or white space.");
+        }
+    }
+}
